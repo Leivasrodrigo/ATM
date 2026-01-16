f3d7c8ec238e1caf3104a93e83e0341f80ce49b0
Leivasrodrigo <digo_130@hotmail.com>
2026-01-15 18:36:24 -0800

    Initial commit

--- src/main/java/com/ATM/Main.java
+++ src/main/java/com/ATM/Main.java
@@ -6,10 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
